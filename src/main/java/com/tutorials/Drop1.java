44842c1d6c641252d3bde697bab23c2365a128d4
sachi1407 <sachinocm23@gmail.com>
2021-01-22 07:56:24 +0530

    sachindropdoewn

---
+++ src/main/java/com/tutorials/Drop1.java
@@ -0,0 +1,23 @@
+package com.tutorials;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Drop1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.testandquiz.com/selenium/testing.html");
+		driver.manage().window().maximize();
+		WebElement web = driver.findElement(By.xpath("//select[@id='testingDropdown']"));
+		Select S=new Select(web);
+		S.selectByVisibleText("Database Testing");
+		
+
+	}
+
+}
